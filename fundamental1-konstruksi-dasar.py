51003dae2a088404abd2dfdfe3da57600038bfa2
Anugrah Kresnaya <anugrahkresnaya.ak@gmail.com>
2020-08-18 22:07:53 +0700

    initial commit

---
+++ fundamental1-konstruksi-dasar.py
@@ -0,0 +1,19 @@
+# KONSTRUKSI DASAR
+# Sequintial: eksekusi Berurutan
+print('Hello World!')
+print('by Anugrah')
+print('-' * 12)
+
+# Percabangan: Eksekusi terpilih
+mau_cepet = True
+
+if mau_cepet:
+    print('Bisa jalan lurus!')
+else:
+    print('Bisa lewat jalan muter!')
+
+# perulangan
+jumlah_anak = 5
+
+for index_anak in range(1, jumlah_anak + 1):
+    print(f'Halo anak {index_anak}')
